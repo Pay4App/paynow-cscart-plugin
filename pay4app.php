d46081a76dff925f90b55917b94279d9ae556613
Sam <sam.takunda@gmail.com>
2015-10-25 07:43:36 +0200

    Handle redirects/notifications from PayNow

--- pay4app.php
+++ pay4app.php
@@ -17,36 +17,52 @@ Credit to //victorkd2@yahoo.com
 * Note the processor_id of the last row in that table
 * Add a new row to the database. CSCart say 3rd party gateways must have their IDs  starting at 1000 and upward.
 * Enter a processor_id of 1000 or greater (one that's not taken)
-* Enter ```Pay4App``` in the 'processor' field
-* ```pay4app.php``` in processor_script
+* Enter ```PayNow``` in the 'processor' field
+* ```paynow.php``` in processor_script
 * ```views/orders/components/payments/cc_outside.tpl``` in 'processor template' (without the quotes)
-* ```pay4app.tpl``` in the admin_template field
+* ```paynow.tpl``` in the admin_template field
 * callback must be ```N``` and type ```P``` (both without the quotes)
-* Browse to your cscart installation folder. Add the pay4app.php file to the folder path app/payments/pay4app.php
-* Also add the pay4app.tpl file to path:
-        ```design/backend/templates/views/payments/components/cc_processors/pay4app.tpl```
+* Browse to your cscart installation folder. Add the paynow.php file to the folder path app/payments/paynow.php
+* Also add the paynow.tpl file to path:
+        ```design/backend/templates/views/payments/components/cc_processors/paynow.tpl```
 
-* In your CSCart admin, go to the 'Administration'->'Payments' menu and pick Pay4App from the list.
+* In your CSCart admin, go to the 'Administration'->'Payments' menu and pick PayNow from the list.
 
-* Enter your merchant details as they are provided you in your Pay4App Merchant dashboard on Pay4App
+* Enter your merchant details as they are provided you in your PayNow account
 
 ****************************************************************************/
 
 use Tygh\Http;
 use Tygh\Registry;
 
+/**
+ * @param $payload Checks if the 'hash' in payload is what we expect
+ */
+public function verifyPayNowHash($payload)
+{
+	$hashString = '';
+	foreach ($payload as $key => $value) {
+		if($key == 'hash') continue;
+		$hashString .= $value;
+	}
+	$hashString = '';
+	$hashString .= $processor_data['processor_params']['integrationkey'];
+	$hashString = strtoupper(hash('sha512', $hashString));
+	return ($hashString === $payload['hash']);
+}
+
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
-// Return from Pay4App's website
+// Return from PayNow's website
 if (defined('PAYMENT_NOTIFICATION')) {
 
     
     if ($mode == 'callback' && !empty($_REQUEST['order'])) {
 
-        if (fn_check_payment_script('pay4app.php', $_REQUEST['order'], $processor_data)) {
+        if (fn_check_payment_script('paynow.php', $_REQUEST['order'], $processor_data)) {
 
             
-            $pay4app_response = array();
+            $paynow_response = array();
 
             if ( !$order_info = fn_get_order_info($_REQUEST['order']) ){
                 die( json_encode( array( 'status'=>0, 'message'=>'Order not found' ) ) );
@@ -58,62 +74,63 @@ if (defined('PAYMENT_NOTIFICATION')) {
                 $processor_data = fn_get_processor_data($order_info['payment_id']);
             }
 
-            $pay4app_statuses   = $processor_data['processor_params']['statuses'];
-            $success_status     = $pay4app_statuses['completed'];
+            $paynow_statuses	= $processor_data['processor_params']['statuses'];
+            $success_status		= $paynow_statuses['completed'];
 
-            // check if valid response: Code from https://pay4app.com/merchants/solutions.php?tab=dvanced 
+            if(!(
+				isset($_POST['reference']) ||
+				isset($_POST['amount']) ||
+				isset($_POST['paynowreference']) ||
+				isset($_POST['pollurl']) ||
+				isset($_POST['status']) ||
+				isset($_POST['hash'])
+			)) return;
+	
+			if (!verifyPayNowHash($_POST)) return;
+	
+			//Verify again with PayNow
+			$pollURL = ''; //@todo Retrieve saved polled URL
+	
+			//Will check only reference and status. But store paynowreference
+			$extra = array(
+		        'headers' => array(
+		            'Connection: close'
+		        ),
+		    );
+		    $response = Http::get($_POST['pollurl'], array(), $extra);
+		    if (empty($response))
+		    {
+		    	return;
+		    }
+		    parse_str($response, $proper_response);
+		    
+		    --
+		    if(!(
+				array_key_exists('reference', $proper_response) &
+				array_key_exists('amount', $proper_response) &
+				array_key_exists('paynowreference', $proper_response) &
+				array_key_exists('pollurl', $proper_response) &
+				array_key_exists('status', $proper_response) &
+				array_key_exists('hash', $proper_response)
+			)) {//response missing expected fields
+				//@todo email request to admin so that they check in PayNow admin since their servers won't repeat this
+				return;
+			if (!verifyPayNowHash($proper_response)) return;
+			if (!$proper_response['status'] == 'ok') return;
             
-            if ( isset($_GET['merchant']) AND isset($_GET['checkout']) AND isset($_GET['order'])
-                  AND isset($_GET['amount']) AND isset($_GET['email']) AND isset($_GET['phone'])
-                  AND isset($_GET['timestamp']) AND isset($_GET['digest']) 
-                ){ 
-
-
-                //for readability the concatenation is split over two lines   
-                $digest = $processor_data['processor_params']['merchantid'].$_GET['checkout'].$_GET['order'].$_GET['amount'];
-                $digest .= $_GET['email'].$_GET['phone'].$_GET['timestamp'].$processor_data['processor_params']['apisecret'];
-
-                $digesthash = hash("sha256", $digest);
-
-                if ($_GET['digest'] !== $digesthash) die( json_encode( array( 'status'=>0 ) ) );
-            
-            } else { die( json_encode( array( 'status'=>0 ) ) ); }
-
-            // end pay4app checks
-
-            
-
+            // End PayNow checks
             if ( fn_format_price($_REQUEST['amount']) != fn_format_price($order_info['total']) ) {
-                $pay4app_response['order_status']       = "Y";
-                $pay4app_response['reason_text']    = 'Pay4App payment and expected order amount mismatch';
-                $pay4app_response['checkout_id']    = $_REQUEST['checkout'];
+                $paynow_response['order_status']  	= "Y";
+                $paynow_response['reason_text']    	= 'PayNow payment and expected order amount mismatch';
+                $paynow_response['checkout_id']    	= $_REQUEST['checkout'];
             
             } else {
-                
-                $pay4app_response['order_status'] = $success_status;
-                $pay4app_response['reason_text'] = 'Payment completed successfully';
-                $pay4app_response['checkout_id'] = $_REQUEST['checkout'];
-                
-
+                $paynow_response['order_status'] = $success_status;
+                $paynow_response['reason_text'] = 'Payment completed successfully';
+                $paynow_response['checkout_id'] = $_REQUEST['checkout'];
             }
-
-
-            if (!empty($_REQUEST['email'])) {
-                $pay4app_response['customer_email'] = $_REQUEST['email'];
-            }
-
-            if (!empty($_REQUEST['phone'])) {
-                $pay4app_response['customer_phone'] = $_REQUEST['phone'];
-            }
-
-            fn_finish_payment($_REQUEST['order'], $pay4app_response);
-
-            if ( $pay4app_response['order_status'] == $success_status ){
-                echo json_encode( array( 'status'=>1 ) ); exit();
-            }
-            else{
-                echo json_encode( array( 'status'=>0 ) ); exit();
-            }
+            
+            fn_finish_payment($_REQUEST['order'], $paynow_response);
             
         }
         exit;
